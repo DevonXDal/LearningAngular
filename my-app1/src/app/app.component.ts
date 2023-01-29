3fcdfc3085716b570f04c291aeb751d7c63687dd
Devon Dalrymple <devonxpersonal@gmail.com>
2023-01-29 14:24:26 -0500

    Added new example but broken project for styling Angular

--- my-app1/src/app/app.component.ts
+++ my-app1/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MyLoggerService } from 'my-logger'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app1';
+
+  constructor(loggerService: MyLoggerService) {
+    loggerService.log('hello world');
+  }
 }
