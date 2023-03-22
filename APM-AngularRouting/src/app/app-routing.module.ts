054be8fd1b5cb18d95c4846cfc9ddf7eb49e7bd8
Devon Dalrymple <devonxpersonal@gmail.com>
2023-03-22 13:41:58 -0400

    Finished Angular routing earlier on, now doing OpenID Connect tutorials

---
+++ APM-AngularRouting/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, PreloadAllModules } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+import { AuthGuard } from './user/auth.guard';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot([
+      {path: 'welcome', component: WelcomeComponent },
+      { path: 'products',
+        canActivate: [AuthGuard],
+        loadChildren: () => import('./products/product.module')
+        .then(m => m.ProductModule)
+      },
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent },
+   ], { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
